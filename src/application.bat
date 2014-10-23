85d02a0868417fdde85359595a4b3497bb6ba60c
romain <romain.toma@imie-rennes.fr>
2014-10-23 13:28:55 +0200

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,5 +1,7 @@
-### application.bat - 201x - votre nom ###
-echo "Copyright toto - 2014"
+### application.bat - 2014 - Toto ###
+version = "2"
+build_hour = "11H00"
 
-echo "version 1.1"
-echo "Build : 11:00"
+echo "Build ${build_hour}"
+echo "version ${version}"
+
